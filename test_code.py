46aa0d54701bb8414cc18a3fee7435297622ac64
TakayukiYamabe-0630 <yamabetakayuki0630@icloud.com>
2021-05-06 13:45:23 +0900

    test

--- test_code.py
+++ test_code.py
@@ -1,7 +1,19 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May  6 13:42:29 2021
-
-@author: ytakayuki
-"""
-
+import matplotlib.pyplot as plt
+ts = 0.01
+g = 9.8 #gravity
+e = 0.6
+v = [] #empty list
+p = []
+t = []
+v.append(0)
+p.append(10)
+t.append(0)
+for i in range(1,1000):
+    t.append(i/100) # time
+    v.append(v[i-1] -g*ts) # velocity
+    p.append(p[i-1] +v[i-1]*ts)
+    if p[i] < 0:
+        p[i] = - e*p[i]
+        v[i] = -e*v[i]
+        plt.plot(t, p)
+        plt.show()
